04da57260063fac39c8c6a68241d494191c87b4c
JDCodes2004 <145573608+JDCodes2004@users.noreply.github.com>
2024-12-17 15:59:21 +0000

    Player Projectile script started (may need help)

--- Assets/Player/Projectiles/Scripts/Player_Bullet.cs
+++ Assets/Player/Projectiles/Scripts/Player_Bullet.cs
@@ -15,6 +15,9 @@ public class Player_Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+
+        }
     }
 }
